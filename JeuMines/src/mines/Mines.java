4f5557c14ba4e4135e5c8b0e01ab69a138653b12
Napi55 <88117366+Napi55@users.noreply.github.com>
2023-05-16 03:45:03 +0200

    Update Mines.java
    Ajouter les commentaires java doc

--- JeuMines/src/mines/Mines.java
+++ JeuMines/src/mines/Mines.java
@@ -5,7 +5,9 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-// Source: http://zetcode.com/tutorials/javagamestutorial/minesweeper/
+/**
+ * La classe Mines est la classe principale du jeu de démineur.
+ */
 
 public class Mines extends JFrame {
 	private static final long serialVersionUID = 4772165125287256837L;
@@ -15,23 +17,51 @@ public class Mines extends JFrame {
 
     private JLabel statusbar;
     
-    public Mines() {
+  /**
+ * Constructeur de la classe Mines.
+ * Initialise la fenêtre du jeu et crée un nouveau plateau de jeu.
+ * @param none
+ * @return none
+ */
+public Mines() {
 
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(WIDTH, HEIGHT);
-        setLocationRelativeTo(null);
-        setTitle("Minesweeper");
+    // Ferme la fenêtre lorsque l'utilisateur clique sur le bouton de fermeture
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        statusbar = new JLabel("");
-        add(statusbar, BorderLayout.SOUTH);
+    // Définit la taille de la fenêtre
+    setSize(WIDTH, HEIGHT);
 
-        add(new Board(statusbar));
+    // Centre la fenêtre sur l'écran
+    setLocationRelativeTo(null);
 
-        setResizable(false);
-        setVisible(true);
-    }
+    // Définit le titre de la fenêtre
+    setTitle("Minesweeper");
+
+    // Crée une étiquette pour afficher le statut du jeu
+    statusbar = new JLabel("");
+    add(statusbar, BorderLayout.SOUTH);
+
+    // Crée un nouveau plateau de jeu et l'ajoute à la fenêtre
+    add(new Board(statusbar));
+
+    // Empêche l'utilisateur de redimensionner la fenêtre
+    setResizable(false);
+
+    // Rend la fenêtre visible
+    setVisible(true);
+}
+
     
-    public static void main(String[] args) {
-        new Mines();
-    }
+   /**
+ * Point d'entrée du programme Minesweeper.
+ * Crée une nouvelle instance de la classe Mines pour lancer le jeu.
+ * @param args un tableau de chaînes de caractères contenant les arguments de la ligne de commande (non utilisé dans ce programme)
+ * @return none
+ */
+public static void main(String[] args) {
+
+    // Crée une nouvelle instance de la classe Mines pour lancer le jeu
+    new Mines();
+}
+
 }
